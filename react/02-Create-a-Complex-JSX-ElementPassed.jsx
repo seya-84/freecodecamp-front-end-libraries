c94b1ff0afc743134bf525d78d83672a6c61d86f
Yannick Severo <yannicksevero@gmail.com>
2021-03-07 09:12:35 +0100

    add solutions react

---
+++ react/02-Create-a-Complex-JSX-ElementPassed.jsx
@@ -0,0 +1,9 @@
+const JSX = <div>
+              <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas, sit.</p>
+              <ul>
+                <li>Lorem, ipsum dolor.</li>
+                <li>Lorem, ipsum dolor.</li>
+                <li>Lorem, ipsum dolor.</li>
+              </ul>
+            </div>;
